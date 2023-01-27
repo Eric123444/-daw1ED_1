d6da82bd101cea396e1dba4dcf0e8c622db385d9
Eric Celma <Eric.celma@ejemplo.com>
2023-01-27 10:46:48 +0100

    Prueba push

---
+++ holamundo.java
@@ -0,0 +1,10 @@
+//Applicacion holamundo de ejemplo
+//
+
+class HolaMundoApp {
+	public static void main ( String args[])
+	{
+	 System.out.println ("Hola mundo!");
+	 System.out.println ("Hola Git!");
+	}
+}
